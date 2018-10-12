95af56d827c452790da0960c3fb66dc3e0559a4a
CJIABKA <cjiabka86@gmail.com>
2018-10-12 15:14:42 +0300

    hehehe

--- main.py
+++ main.py
@@ -3,49 +3,65 @@
 import xlrd
 import xlwt
 import os
-#from math import ceil
-#from xlutils.copy import copy
 #import re
 
-def writeROW(i, data):    
+def writeROW(i, data, ws):
     for cell in range(len(data)):        
         ws.write(i, cell, data[cell])
 
-f01 = [u'MER-0' + str(x) for x in range(10,30)]
-f02 = [u'MER-' + str(x) for x in range(400,456)] 
-filter01 = [u'LER-014'] + f01 + f02 + [u'MER-501']
-print filter01
-filter02 = [u'UNGG']
-
 pathos = os.getcwd()
-xlsdir = pathos + '\\xls'
-wb = xlwt.Workbook()
-ws = wb.add_sheet(u'Result')
-i  = 1
-for xls in os.listdir(xlsdir):    
-    if xls[-4:] == '.xls':
+
+matchtablexls = pathos + '\\WorkPlace\\MatchTable.xls'
+rb = xlrd.open_workbook(matchtablexls)
+sheet = rb.sheet_by_index(0)
+matchtable = {}
+old_names = []
+for row in range(sheet.nrows):
+    old = sheet.cell_value(row, 0)
+    new = sheet.cell_value(row, 1)
+    old_names.append(old)
+    matchtable[old] = new
+#print matchtable
+#print len(matchtable)
+
+xlsdir = pathos + '\\WorkPlace\\xls'
+i = 1
+for xls in os.listdir(xlsdir):
+    if xls.split('.')[-1] == 'xls' and xls[:4] != 'new_':
+        wb_new = xlwt.Workbook()
+        ws_new = wb_new.add_sheet(xls.split('.')[1])
+        wb_rem = xlwt.Workbook()
+        ws_rem = wb_rem.add_sheet(xls.split('.')[1])
         print '***********************',xls   
         rb = xlrd.open_workbook(xlsdir + '\\' + xls)
         sheet = rb.sheet_by_index(0)
-        count = 0
         ncols = sheet.ncols
-        for row in range(sheet.nrows):
-            count += 1
-            #if count < 5416 or count>5430:
-            #    continue
-            rawrow = []
+        rawrow = []
+        for col in range(ncols):
+            cell = sheet.cell_value(0, col)
+            rawrow.append(cell)
+        writeROW(0, rawrow, ws_new)
+        writeROW(0, rawrow, ws_rem)
+
+        for row in range(1, sheet.nrows):
+            addrowflag = False
+            rawrow_new = []
+            rawrow_rem = []
             for col in range(ncols):
-                cell = sheet.cell_value(row, col)
-                rawrow.append(cell)
-            #print rawrow
-            if (unicode(rawrow[14].strip()) in filter02) and (unicode(rawrow[30].strip()) in filter01):
-                writeROW(i, rawrow)
-                i+=1
-            #else:
-            #    print rawrow[14], rawrow[30]
-            #    print rawrow[14].strip() in filter02, rawrow[30] in filter01                
-            if count%100==0:
-                print count
+                cell_rem = sheet.cell_value(row, col)
+                cell_new = cell_rem
+                if cell_new in old_names:
+                    cell_new = matchtable[cell_new]
+                    addrowflag = True
+                rawrow_rem.append(cell_rem)
+                rawrow_new.append(cell_new)
+            if addrowflag:
+                writeROW(i, rawrow_new, ws_new)
+                writeROW(i, rawrow_rem, ws_rem)
+                i += 1
+
+        wb_new.save(xlsdir + '\\new_' + xls)
+        wb_rem.save(xlsdir + '\\rem_' + xls)
         
-wb.save('result.xls')
+
 print u'Обработка закончена'
